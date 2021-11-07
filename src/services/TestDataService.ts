6cd8fc8635d58db3768d10b9d7960805fe0d6abd
ArtMalina <art.malina@gmail.com>
2021-11-07 19:33:27 +0300

    feat: new buying behavior

--- src/services/TestDataService.ts
+++ src/services/TestDataService.ts
@@ -1,5 +1,19 @@
 ﻿import { FirebaseApp, initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, getDoc, updateDoc, addDoc, Firestore, CollectionReference, doc } from 'firebase/firestore/lite';
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    getDoc,
+    updateDoc,
+    addDoc,
+    Firestore,
+    CollectionReference,
+    doc,
+    query,
+    where,
+    QuerySnapshot,
+    DocumentData,
+} from 'firebase/firestore/lite';
 import testFirebaseConfig from './testDbConfig';
 
 import { BehaviorSubject } from "rxjs/internal/BehaviorSubject";
@@ -16,7 +30,7 @@ import {
 } from "./interfaces";
 import { fromContractTile } from "./mappers";
 
-const TEST_OWNER_ADDR = "test-owner-3" as AccountAddr;
+const TEST_OWNER_ADDR = "test-owner-2" as AccountAddr;
 
 const ERROR_TOKEN: ContractTokenInfo = {
     _id: 1,
@@ -153,9 +167,21 @@ export default class DataService implements IDataService {
 
         try {
             await Promise.all(
-                sortedTiles.map((tileData, i) =>
-                    updateDoc(doc(this._db, 'tiles', tileData.tile._id), tileToFirebaseMapper({ ...tileData.tile, url: !i ? groupUrl : '', boundedTiles: !i ? sortedTiles.map(k => k.tile.id) : [] }))
-                )
+                sortedTiles.map((tileData, i) => {
+                    const ifParentBounds = groupUrl && !i ? [...sortedTiles.map(k => k.tile.id)] : [];
+                    const ifInGroupTileBounds = groupUrl && !!i ? [sortedTiles[0].tile.id] : []
+                    return updateDoc(
+                        doc(
+                            this._db, 'tiles',
+                            tileData.tile._id
+                        ),
+                        tileToFirebaseMapper({
+                            ...tileData.tile,
+                            url: !i ? groupUrl : '',
+                            boundedTiles: !i ? [...ifParentBounds] : [...ifInGroupTileBounds]
+                        })
+                    )
+                })
             );
             await this.fetchTiles();
             return [true, ''];
@@ -177,13 +203,72 @@ export default class DataService implements IDataService {
         if (!currAcc) return [false, 'no current account'];
 
         try {
-            await Promise.all(
-                tiles.map((tileData, i) =>
-                    updateDoc(doc(this._db, 'tiles', tileData.tile._id), tileToFirebaseMapper({ ...tileData.tile, url, owner: currAcc }))
-                )
+            const snapShots = await Promise.all(
+                tiles.map(tData => getDoc(doc(this._db, 'tiles', tData.tile._id)))
             );
+
+            console.log('snapshots', snapShots);
+
+            const storedTiles = snapShots.map(t => tileFirebaseMapper({ ...t.data(), _id: t.id }));
+            // get parent tiles (if we buy not parent, but grouped tiles)
+            const parentTilesTupleIds = storedTiles.reduce<[ContractTileID, ContractTileID][]>(
+                (acc, tile) => {
+                    if (tile.boundedTiles.length && tile.boundedTiles[0] !== tile.id) {
+                        return [...acc, [tile.boundedTiles[0], tile.id]];
+                    }
+                    return [...acc];
+                }, []
+            );
+            const parentStoredTilesSnapshot = await Promise.all<[QuerySnapshot<DocumentData>, ContractTileID]>(
+                parentTilesTupleIds.map(([parenTileId, buyingTileId]) => {
+                    return new Promise(async (resolve, reject) => {
+                        try {
+                            const doc = await getDocs(query(
+                                this._tilesCollection, where('id', '==', '' + parenTileId))
+                            );
+                            resolve([doc, buyingTileId]);
+                        } catch (error) {
+                            reject(error);
+                        }
+                    });
+                })
+            );
+
+            const parentStoredTilesForUpd = parentStoredTilesSnapshot.map<null | [ContractTileInfo, ContractTileID]>(([t, buyingTileId]) => {
+                const docs = t.docs.map(doc => tileFirebaseMapper({ ...doc.data(), _id: doc.id }));
+                const tile = docs[0];
+                if (!tile.boundedTiles.find(x => x === buyingTileId)) return null;
+                return [docs[0], buyingTileId];
+            }).filter(t => !!t) as [ContractTileInfo, ContractTileID][];
+
+            console.log('parent store tiles: ', parentStoredTilesForUpd);
+
+            await Promise.all(
+                parentStoredTilesForUpd.map(([t, buyingTileId]) => updateDoc(
+                    doc(this._db, 'tiles', t._id),
+                    tileToFirebaseMapper({
+                        ...t,
+                        boundedTiles: t.boundedTiles.filter(x => x !== buyingTileId)
+                    })
+                ))
+            );
+
+            await Promise.all(
+                tiles.map((tileData, i) => {
+                    const storedTile = storedTiles.find(t => t._id === tileData.tile._id);
+                    if (!storedTile) return Promise.reject(`no tile found (# ${tileData.cellNumber})`);
+
+                    return updateDoc(
+                        doc(this._db, 'tiles', tileData.tile._id),
+                        tileToFirebaseMapper({ ...tileData.tile, url, owner: currAcc, boundedTiles: [] })
+                    )
+                })
+            );
+
             await this.fetchTiles();
+
             return [true, ''];
+
         } catch (error) {
             throw error;
         }
