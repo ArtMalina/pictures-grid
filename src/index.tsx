77d28d77b92086dd07c439a296697d97a475a5a2
ArtMalina <art.malina@gmail.com>
2023-03-22 01:04:28 +0300

    update: use vite instead

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.scss";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<App />);
